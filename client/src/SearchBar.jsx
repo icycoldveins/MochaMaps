7f5627fe7c8fe39710f6a5b22040bee516be4f12
Kevin Wijaya <kevinwijaya306@gmail.com>
2023-12-27 11:57:24 -0500

    changed sizing again for mobile

--- client/src/SearchBar.jsx
+++ client/src/SearchBar.jsx
@@ -1,7 +1,7 @@
 import "mapbox-gl/dist/mapbox-gl.css";
 import React, { useState } from "react";
 import axios from "axios";
-
+import "./searchbar.css";
 
 const SearchBar = ({ onSearch }) => {
   const [query, setQuery] = useState("");
@@ -16,8 +16,10 @@ const SearchBar = ({ onSearch }) => {
     try {
       // Call Mapbox API to get coordinates
       const mapboxResponse = await axios.get(
-        `https://api.mapbox.com/geocoding/v5/mapbox.places/${query}.json?access_token=${import.meta.env.VITE_APP_MAPBOX_API_KEY}`
-        );
+        `https://api.mapbox.com/geocoding/v5/mapbox.places/${query}.json?access_token=${
+          import.meta.env.VITE_APP_MAPBOX_API_KEY
+        }`
+      );
       const coordinates = mapboxResponse.data.features[0].center;
       console.log("Mapbox coordinates:", coordinates);
 
@@ -48,40 +50,18 @@ const SearchBar = ({ onSearch }) => {
     }
   };
 
+  const isMobile = window.innerWidth <= 600;
+
   return (
-    <form
-      onSubmit={handleSubmit}
-      style={{ display: "flex", justifyContent: "center", marginTop: "50px" }}
-    >
+    <form onSubmit={handleSubmit} className="search-bar">
       <input
         type="text"
         value={query}
         onChange={handleInputChange}
         placeholder="Enter zipcode or location"
-        style={{
-          marginRight: "10px",
-          padding: "10px",
-          fontSize: "1em",
-          border: "2px solid #0000ff",
-          borderRadius: "5px",
-          width: "100%", // Add this line
-          maxWidth: "300px", // And this line
-          outline: "none",
-        }}
+        className="search-input"
       />
-      <button
-        type="submit"
-        style={{
-          padding: "10px 20px",
-          fontSize: "1em",
-          border: "none",
-          borderRadius: "5px",
-          backgroundColor: "#0000ff",
-          color: "#fff",
-          cursor: "pointer",
-          outline: "none",
-        }}
-      >
+      <button type="submit" className="search-button">
         Search
       </button>
     </form>
